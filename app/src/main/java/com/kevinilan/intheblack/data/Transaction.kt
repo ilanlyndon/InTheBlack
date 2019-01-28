e5df3eebda43f27cfb8c1d27eb734a347ac0e0e9
Lesigh3100 <kevin.cheney2000@gmail.com>
2019-01-28 21:27:28 +0200

    Added a larger size version of transaction_add_item, added a few Strings.

--- app/src/main/java/com/kevinilan/intheblack/data/Transaction.kt
+++ app/src/main/java/com/kevinilan/intheblack/data/Transaction.kt
@@ -2,11 +2,11 @@ package com.kevinilan.intheblack.data
 
 
 data class Transaction(
-    val transactionName: String,
+    val transactionName: String?,
     val transactionValue: Double,
     val transactionType: TransactionType,
-    val currencyType: String,
-    val transactionMaker: String,
+    val currencyType: String?,
+    val transactionMaker: String?,
     val timesOfTransaction: Any?,
     val locationOfTransaction: Any?
 )
