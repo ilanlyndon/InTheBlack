3ef3e72cac279e75d48fbb076fc472b0f3e84480
Lesigh3100 <kevin.cheney2000@gmail.com>
2019-02-08 17:03:41 +0200

    Added methods to create JSON from our objects, and get the objects back from JSON, as well as Unit Tests for them.

--- app/src/main/java/com/kevinilan/intheblack/utils/JSONutils.kt
+++ app/src/main/java/com/kevinilan/intheblack/utils/JSONutils.kt
@@ -1,2 +1,37 @@
 package com.kevinilan.intheblack.utils
 
+import com.google.gson.GsonBuilder
+import com.kevinilan.intheblack.data.Budget
+import com.kevinilan.intheblack.data.BudgetItem
+import com.kevinilan.intheblack.data.Ledger
+import com.kevinilan.intheblack.data.Transaction
+
+
+fun objectToJSON(toJSONObject: Any): Any {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+    return gson.toJson(toJSONObject)
+}
+
+fun transactionFromJSON(fromJSONObject: String): Any {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    return gson.fromJson(fromJSONObject, Transaction::class.java)
+}
+
+fun ledgerFromJSON(fromJSONObject: String): Any {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    return gson.fromJson(fromJSONObject, Ledger::class.java)
+}
+
+fun budgetItemFromJSON(fromJSONObject: String): Any {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    return gson.fromJson(fromJSONObject, BudgetItem::class.java)
+}
+
+fun budgetFromJSON(fromJSONObject: String): Any {
+    val gson = GsonBuilder().setPrettyPrinting().create()
+
+    return gson.fromJson(fromJSONObject, Budget::class.java)
+}
